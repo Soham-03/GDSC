8bdb290427ddf6a11c5d36ae1bd91d9e7ce6b2df
Soham-03 <sohamparab1003@gmail.com>
2023-08-13 20:38:24 +0530

    Created LogInSignupActivity.kt Activity and created a composable for the activity screen

---
+++ app/src/main/java/com/soham/gdsc/model/Event.kt
@@ -0,0 +1,9 @@
+package com.soham.gdsc.model
+
+data class Event(
+    val eventId: String,
+    val eventName: String,
+    val eventDate: String,
+    val eventTime: String,
+    val eventImage: String
+)
---
+++ app/src/main/java/com/soham/gdsc/ui/screen/SignInScreen.kt
@@ -0,0 +1,246 @@
+package com.soham.gdsc.ui.screen
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.soham.gdsc.R
+import com.soham.gdsc.ui.theme.*
+
+@Composable
+fun SignInScreen(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    )
+    {
+        //background elements
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+        )
+        {
+            Image(
+                painter = painterResource(id = R.drawable.ic_gdsc_logo),            contentDescription = "GDSC Logo",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .height(80.dp)
+                    .align(Alignment.Start)
+            )
+            Box(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+            )
+            {
+                var height by remember{ mutableStateOf(0.dp) }
+                val localDensity = LocalDensity.current
+                Box(modifier = Modifier
+                    .padding(top = 8.dp)
+                    .fillMaxWidth()
+                    .height(height)
+                    .border(2.dp, Color.Black, RoundedCornerShape(30.dp))
+                    .background(cardBackgroundGreen, RoundedCornerShape(30.dp))
+                )
+                Card(
+                    backgroundColor = Color.White,
+                    border = BorderStroke(2.dp, Color.Black),
+                    shape = RoundedCornerShape(30.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .onGloballyPositioned { coordinates ->
+                            height = with(localDensity) { coordinates.size.height.toDp() }
+                        }
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .fillMaxWidth()
+                    )
+                    {
+                        var name by remember{ mutableStateOf(TextFieldValue("")) }
+                        Text(
+                            text = "Hello, Developers",
+                            color = textColorGrey,
+                            fontSize = 16.sp
+                        )
+                        Text(
+                            text = "Complete Your Profile & Sign Up <3",
+                            color = textColorGrey,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        OutlinedTextField(
+                            value = name,
+                            onValueChange = {
+                                name = it
+                            },
+                            shape = RoundedCornerShape(30.dp),
+                            label = {
+                                Text(
+                                    text = "What is your name?",
+                                    color = textColorGrey,
+                                )
+                            },
+                            colors = TextFieldDefaults.outlinedTextFieldColors(
+                                textColor = textColorGrey,
+                                focusedBorderColor = LightBlue
+                            ),
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Text
+                            ),
+                            modifier = Modifier
+                                .padding(top = 12.dp)
+                                .fillMaxWidth()
+                                .align(Alignment.CenterHorizontally)
+                        )
+                        OutlinedTextField(
+                            value = name,
+                            onValueChange = {
+                                name = it
+                            },
+                            shape = RoundedCornerShape(30.dp),
+                            label = {
+                                Text(
+                                    text = "Your Roll No.?",
+                                    color = textColorGrey,
+                                )
+                            },
+                            colors = TextFieldDefaults.outlinedTextFieldColors(
+                                textColor = textColorGrey,
+                                focusedBorderColor = LightBlue
+                            ),
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Number
+                            ),
+                            modifier = Modifier
+                                .padding(top = 12.dp)
+                                .fillMaxWidth()
+                                .align(Alignment.CenterHorizontally)
+                        )
+                        OutlinedTextField(
+                            value = name,
+                            onValueChange = {
+                                name = it
+                            },
+                            shape = RoundedCornerShape(30.dp),
+                            label = {
+                                Text(
+                                    text = "Your Class?",
+                                    color = textColorGrey,
+                                )
+                            },
+                            colors = TextFieldDefaults.outlinedTextFieldColors(
+                                textColor = textColorGrey,
+                                focusedBorderColor = LightBlue
+                            ),
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Text
+                            ),
+                            modifier = Modifier
+                                .padding(top = 12.dp)
+                                .fillMaxWidth()
+                                .align(Alignment.CenterHorizontally)
+                        )
+
+                        Box(
+                            modifier = Modifier
+                                .padding(top = 12.dp)
+                        )
+                        {
+//                    Card(
+//                        backgroundColor = Yellow,
+//                        shape = RoundedCornerShape(30.dp),
+//                        border = BorderStroke(2.dp, Color.Black),
+//                        modifier = Modifier
+//                            .padding(start = 20.dp, bottom = 8.dp)
+//                            .fillMaxWidth()
+//                            .height(60.dp)
+//                    )
+//                    {
+//
+//                    }
+                            Box(modifier = Modifier
+                                .padding(top = 8.dp)
+                                .fillMaxWidth()
+                                .height(60.dp)
+                                .border(2.dp, Color.Black, RoundedCornerShape(30.dp))
+                                .background(Yellow, RoundedCornerShape(30.dp))
+                            )
+                            Box(contentAlignment = Alignment.Center,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(60.dp)
+                                    .border(2.dp, Color.Black, RoundedCornerShape(30.dp))
+                                    .background(Color.White, RoundedCornerShape(30.dp))
+                            )
+                            {
+                                Text(
+                                    text = "Continue with Google",
+                                    color = textColorGrey,
+                                    fontSize = 24.sp
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+            Text(
+                text = "Already have an account?",
+                fontSize = 14.sp,
+                color = LightRed,
+                modifier = Modifier
+                    .padding(top = 20.dp, bottom = 8.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+            Button(
+                shape = RoundedCornerShape(30.dp),
+                border = BorderStroke(2.dp, Color.Black),
+                onClick = {
+
+                },
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Yellow,
+                    contentColor = Color.Black
+                ),
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+            ) {
+                Text(text = "Log In with Google", fontSize = 18.sp)
+            }
+            Image(
+                painter = painterResource(id = R.drawable.bg_bottom),
+                contentDescription = "bg Bottom",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp)
+            )
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun SignInPreview(){
+    SignInScreen()
+}
